c97fe0c8047dc579e58fdc04405a6b4d991cf329
TechWarrior2023 <140419054+ItcTrainee2023@users.noreply.github.com>
2023-09-05 20:00:38 +0100

    Update 02-variables.tf
    changed to central canada

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,7 +7,7 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central US"
+  default = "Central Canada"
 }
 
 # Azure Resource Group Name
